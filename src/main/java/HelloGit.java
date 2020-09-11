53f6a606b6e3fb8912933b1e2ab29c65dc5f0fe3
avatargirase2 <divyarajput62@gmail.com>
2020-09-11 12:59:36 -0700

    This is comitted through Intellij

--- src/main/java/HelloGit.java
+++ src/main/java/HelloGit.java
@@ -1,7 +1,7 @@
 // This is done and now upodated by forked User usin intellij
 public class HelloGit {
 	
-	// Commented
+	// Commented  by intellij
 	public static void main(String[] args) {
 		System.out.println("Hi fi in test branch");
 	}
